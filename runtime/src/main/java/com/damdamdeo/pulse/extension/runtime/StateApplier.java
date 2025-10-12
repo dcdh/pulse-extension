770df751c3b87e4d34ec2448aae3b9877bd6889a
damien <damien.clementdhuart@gmail.com>
2025-10-12 11:38:29 +0200

    use a cache at instance level for events callback

--- runtime/src/main/java/com/damdamdeo/pulse/extension/runtime/StateApplier.java
+++ runtime/src/main/java/com/damdamdeo/pulse/extension/runtime/StateApplier.java
@@ -1,9 +1,8 @@
 package com.damdamdeo.pulse.extension.runtime;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
 abstract class StateApplier<A extends AggregateRoot<K>, K extends AggregateId<?>> implements EventAppender<K> {
 
@@ -11,12 +10,25 @@ abstract class StateApplier<A extends AggregateRoot<K>, K extends AggregateId<?>
 
     private final A aggregate;
     private final List<Event<K>> newEvents;
+    private final Map<Class<Event<?>>, Method> cacheEventMethods;
 
     public StateApplier(final AggregateRootInstanceCreator aggregateRootInstanceCreator,
                         final List<Event<K>> events) {
         Objects.requireNonNull(aggregateRootInstanceCreator);
         Objects.requireNonNull(events);
         this.aggregate = aggregateRootInstanceCreator.create(getAggregateClass());
+        // TODO will be created each time a StateApplier is created thus each time an Aggregate is created, loaded
+        // This should be defined globally elsewhere
+        this.cacheEventMethods = Arrays.stream(this.aggregate.getClass().getDeclaredMethods())
+                .filter(m -> EVENT_HANDLER_METHOD_NAMING.equals(m.getName()))
+                .filter(m -> m.getParameterCount() == 1)
+                .filter(m -> Event.class.isAssignableFrom(m.getParameterTypes()[0]))
+                .filter(m -> m.canAccess(aggregate))
+                .collect(Collectors.toMap(
+                        m -> (Class<Event<?>>) m.getParameterTypes()[0],
+                        m -> m,
+                        (m1, m2) -> m1
+                ));
         events.forEach(this::apply);
         this.newEvents = new ArrayList<>();
     }
@@ -35,20 +47,13 @@ abstract class StateApplier<A extends AggregateRoot<K>, K extends AggregateId<?>
         if (aggregateId != null && !event.id().equals(aggregateId)) {
             throw new IllegalStateException("Applying event on an aggregate with different id.");
         }
-        // TODO use a cache mechanism
-        Arrays.stream(aggregate.getClass().getDeclaredMethods())
-                .filter(m -> EVENT_HANDLER_METHOD_NAMING.equals(m.getName()))
-                .filter(m -> m.getParameterCount() == 1)
-                .filter(m -> m.getParameterTypes()[0].isAssignableFrom(event.getClass()))
-                .filter(m -> m.canAccess(aggregate))
-                .findFirst()
-                .ifPresent(m -> {
-                    try {
-                        m.invoke(aggregate, event);
-                    } catch (Exception e) {
-                        throw new RuntimeException("Error invoking event sourcing handler", e);
-                    }
-                });
+        if (this.cacheEventMethods.containsKey(event.getClass())) {
+            try {
+                this.cacheEventMethods.get(event.getClass()).invoke(aggregate, event);
+            } catch (Exception e) {
+                throw new RuntimeException("Error invoking event sourcing handler", e);
+            }
+        }
     }
 
     A aggregate() {
