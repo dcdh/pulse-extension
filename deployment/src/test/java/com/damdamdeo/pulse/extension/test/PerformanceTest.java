982dff452333eacfb00af4951f89a328014b33c7
damien <damien.clementdhuart@gmail.com>
2025-10-28 23:24:26 +0100

    work on performance - use HdrHistogram to interpret responses time

--- deployment/src/test/java/com/damdamdeo/pulse/extension/test/PerformanceTest.java
+++ deployment/src/test/java/com/damdamdeo/pulse/extension/test/PerformanceTest.java
@@ -13,6 +13,7 @@ import io.quarkus.cache.CacheName;
 import io.quarkus.test.QuarkusUnitTest;
 import io.smallrye.context.api.ManagedExecutorConfig;
 import jakarta.inject.Inject;
+import org.HdrHistogram.Histogram;
 import org.apache.commons.lang3.time.StopWatch;
 import org.eclipse.microprofile.context.ManagedExecutor;
 import org.junit.jupiter.api.Disabled;
@@ -27,7 +28,6 @@ import java.sql.SQLException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
-import java.util.LongSummaryStatistics;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -165,15 +165,15 @@ class PerformanceTest {
         final List<Long> executionsIn = futures.stream()
                 .map(CompletableFuture::join)
                 .toList();
-        final LongSummaryStatistics stats = LongStream.range(0, executionsIn.size())
-                .map(i -> executionsIn.get((int) i))
-                .summaryStatistics();
+        final Histogram histogram = new Histogram(5);
+        executionsIn.forEach(histogram::recordValue);
+
         LOGGER.info("Fetch Performance");
-        LOGGER.info("count: %d".formatted(stats.getCount()));
-        LOGGER.info("sum: %d in ms".formatted(stats.getSum()));
-        LOGGER.info("min: %d in ms".formatted(stats.getMin()));
-        LOGGER.info("average: %f in ms".formatted(stats.getAverage()));
-        LOGGER.info("max: %d in ms".formatted(stats.getMax()));
+        LOGGER.info("min: %d in ms".formatted(histogram.getMinValue()));
+        LOGGER.info("P50: %d in ms".formatted(histogram.getValueAtPercentile(50)));
+        LOGGER.info("P90: %d in ms".formatted(histogram.getValueAtPercentile(90)));
+        LOGGER.info("P99: %d in ms".formatted(histogram.getValueAtPercentile(99)));
+        LOGGER.info("max: %d in ms".formatted(histogram.getMaxValue()));
 
         try (final Connection connection = dataSource.getConnection();
              final PreparedStatement perfEventTablePreparedStatement = connection.prepareStatement(
@@ -210,46 +210,46 @@ class PerformanceTest {
         }
     }
 /*
-2025-10-28 22:51:37,414 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Start execute performance test
-2025-10-28 22:51:37,414 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Create 30.000 of NewTodoCreated events
-2025-10-28 22:51:37,415 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 0
-2025-10-28 22:51:44,860 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 1000
-2025-10-28 22:51:52,086 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 2000
-2025-10-28 22:51:59,001 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 3000
-2025-10-28 22:52:08,959 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 4000
-2025-10-28 22:52:16,547 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 5000
-2025-10-28 22:52:23,634 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 6000
-2025-10-28 22:52:30,073 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 7000
-2025-10-28 22:52:37,582 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 8000
-2025-10-28 22:52:47,372 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 9000
-2025-10-28 22:52:54,892 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 10000
-2025-10-28 22:53:02,011 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 11000
-2025-10-28 22:53:09,243 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 12000
-2025-10-28 22:53:17,500 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 13000
-2025-10-28 22:53:27,430 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 14000
-2025-10-28 22:53:35,360 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 15000
-2025-10-28 22:53:42,421 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 16000
-2025-10-28 22:53:49,957 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 17000
-2025-10-28 22:53:56,915 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 18000
-2025-10-28 22:54:06,285 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 19000
-2025-10-28 22:54:12,121 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 20000
-2025-10-28 22:54:18,360 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 21000
-2025-10-28 22:54:23,903 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 22000
-2025-10-28 22:54:31,395 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 23000
-2025-10-28 22:54:39,650 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 24000
-2025-10-28 22:54:46,849 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 25000
-2025-10-28 22:54:53,163 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 26000
-2025-10-28 22:55:00,120 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 27000
-2025-10-28 22:55:08,691 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 28000
-2025-10-28 22:55:14,825 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 29000
-2025-10-28 22:55:21,820 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) 30.000 of NewTodoCreated events created in 224405 ms
-2025-10-28 22:55:24,053 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Fetch Performance
-2025-10-28 22:55:24,054 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) count: 1000
-2025-10-28 22:55:24,054 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) sum: 162534 in ms
-2025-10-28 22:55:24,054 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) min: 6 in ms
-2025-10-28 22:55:24,054 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) average: 162,534000 in ms
-2025-10-28 22:55:24,054 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) max: 2184 in ms
-2025-10-28 22:55:24,056 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) t_event - table_size '23 MB' - indexes_size '9112 kB' - total_size '32 MB'
-2025-10-28 22:55:24,056 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) t_event - table_size '23 MB' - indexes_size '4336 kB' - total_size '28 MB'
+2025-10-28 23:04:26,303 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Start execute performance test
+2025-10-28 23:04:26,304 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Create 30.000 of NewTodoCreated events
+2025-10-28 23:04:26,305 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 0
+2025-10-28 23:04:31,745 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 1000
+2025-10-28 23:04:39,333 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 2000
+2025-10-28 23:04:44,283 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 3000
+2025-10-28 23:04:48,526 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 4000
+2025-10-28 23:04:53,505 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 5000
+2025-10-28 23:04:58,601 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 6000
+2025-10-28 23:05:08,204 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 7000
+2025-10-28 23:05:13,937 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 8000
+2025-10-28 23:05:19,531 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 9000
+2025-10-28 23:05:25,735 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 10000
+2025-10-28 23:05:33,095 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 11000
+2025-10-28 23:05:40,857 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 12000
+2025-10-28 23:05:47,504 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 13000
+2025-10-28 23:05:53,548 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 14000
+2025-10-28 23:06:00,107 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 15000
+2025-10-28 23:06:09,102 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 16000
+2025-10-28 23:06:16,018 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 17000
+2025-10-28 23:06:22,619 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 18000
+2025-10-28 23:06:29,064 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 19000
+2025-10-28 23:06:35,603 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 20000
+2025-10-28 23:06:46,125 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 21000
+2025-10-28 23:06:52,577 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 22000
+2025-10-28 23:06:58,988 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 23000
+2025-10-28 23:07:05,597 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 24000
+2025-10-28 23:07:12,455 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 25000
+2025-10-28 23:07:22,707 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 26000
+2025-10-28 23:07:29,438 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 27000
+2025-10-28 23:07:36,424 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 28000
+2025-10-28 23:07:43,622 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Current creation 29000
+2025-10-28 23:07:50,591 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) 30.000 of NewTodoCreated events created in 204285 ms
+2025-10-28 23:07:52,251 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) Fetch Performance
+2025-10-28 23:07:52,251 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) min: 7 in ms
+2025-10-28 23:07:52,251 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) P50: 20 in ms
+2025-10-28 23:07:52,251 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) P90: 67 in ms
+2025-10-28 23:07:52,252 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) P99: 1527 in ms
+2025-10-28 23:07:52,252 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) max: 1604 in ms
+2025-10-28 23:07:52,253 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) t_event - table_size '23 MB' - indexes_size '9112 kB' - total_size '32 MB'
+2025-10-28 23:07:52,253 INFO  [com.dam.pul.ext.tes.PerformanceTest] (main) t_event - table_size '23 MB' - indexes_size '4336 kB' - total_size '28 MB'
  */
 }
