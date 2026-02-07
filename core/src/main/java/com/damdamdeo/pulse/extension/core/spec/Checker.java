eeb2fb8eb91cf577adeb21171881be9143cfb06e
damien <damien.clementdhuart@gmail.com>
2026-02-07 17:56:27 +0100

    add checker useful to throw business exception when a part is not satisfied

---
+++ core/src/main/java/com/damdamdeo/pulse/extension/core/spec/Checker.java
@@ -0,0 +1,37 @@
+package com.damdamdeo.pulse.extension.core.spec;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+// Chain of responsibility pattern.
+// The first Specification returning false will throw the supplied exception.
+public final class Checker<T> {
+
+    private final List<Step<T>> steps = new ArrayList<>();
+
+    private record Step<T>(Specification<T> spec, Function<T, ? extends RuntimeException> exceptionProvider) {
+
+        private Step {
+            Objects.requireNonNull(spec);
+            Objects.requireNonNull(exceptionProvider);
+        }
+    }
+
+    public Checker<T> next(final Specification<T> spec, final Function<T, ? extends RuntimeException> exceptionProvider) {
+        Objects.requireNonNull(spec);
+        Objects.requireNonNull(exceptionProvider);
+        steps.add(new Step<>(spec, exceptionProvider));
+        return this;
+    }
+
+    public void check(final T t) {
+        Objects.requireNonNull(t);
+        for (final Step<T> step : steps) {
+            if (!step.spec.isSatisfiedBy(t)) {
+                throw step.exceptionProvider.apply(t);
+            }
+        }
+    }
+}
---
+++ core/src/test/java/com/damdamdeo/pulse/extension/core/spec/CheckerTest.java
@@ -0,0 +1,48 @@
+package com.damdamdeo.pulse.extension.core.spec;
+
+import com.damdamdeo.pulse.extension.core.Status;
+import com.damdamdeo.pulse.extension.core.Todo;
+import com.damdamdeo.pulse.extension.core.TodoId;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CheckerTest {
+
+    private static final Checker<Todo> IMPORTANT_NEXT_IN_PROGRESS = new Checker<Todo>()
+            .next(new TodoIsImportantSpec(), (todo) -> new IllegalStateException("la todo %s doit être importante".formatted(todo.id().id())))
+            .next(new TodoIsInProgressSpec(), (todo) -> new IllegalStateException("la todo %s doit être in progress".formatted(todo.id().id())));
+
+    @Test
+    void shouldNotFailWhenTodoIsImportantAndInProgress() {
+        // Given
+        final Todo givenTodo = new Todo(new TodoId("Damien", 0L), "lorem", Status.IN_PROGRESS, true);
+
+        // When && Then
+        assertThatCode(() -> IMPORTANT_NEXT_IN_PROGRESS.check(givenTodo)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void shouldFailWhenTodoIsNotImportant() {
+        // Given
+        final Todo givenTodo = new Todo(new TodoId("Damien", 0L), "lorem", Status.IN_PROGRESS, false);
+
+        // When && Then
+        assertThatThrownBy(() -> IMPORTANT_NEXT_IN_PROGRESS.check(givenTodo))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("la todo Damien/0 doit être importante");
+    }
+
+    @Test
+    void shouldFailWhenTodoIsImportantAndNotInProgress() {
+        // Given
+        final Todo givenTodo = new Todo(new TodoId("Damien", 0L), "lorem", Status.DONE, true);
+
+        // When && Then
+        assertThatThrownBy(() -> IMPORTANT_NEXT_IN_PROGRESS.check(givenTodo))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("la todo Damien/0 doit être in progress");
+    }
+
+}
