ee00db93174d9858261cdd76b435d10b6a6fba52
damien <damien.clementdhuart@gmail.com>
2026-05-14 23:29:20 +0200

    add test on produce sequences in JdbcPostgresSequenceGeneratorTest

--- writer/deployment/src/test/java/com/damdamdeo/pulse/extension/writer/deployment/JdbcPostgresSequenceGeneratorTest.java
+++ writer/deployment/src/test/java/com/damdamdeo/pulse/extension/writer/deployment/JdbcPostgresSequenceGeneratorTest.java
@@ -9,6 +9,15 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -24,6 +33,9 @@ public class JdbcPostgresSequenceGeneratorTest {
 
     @Inject
     JdbcPostgresSequenceGenerator jdbcPostgresSequenceGenerator;
+
+    @Inject
+    DataSource dataSource;
 
     record TodoId() implements AggregateId {
 
@@ -42,7 +54,7 @@ public class JdbcPostgresSequenceGeneratorTest {
     }
 
     @Test
-    void shouldCreateASequence() throws SequenceGenerationException {
+    void shouldCreateSequences() throws SequenceGenerationException {
         // Given
         final Class<? extends AggregateId> given = TodoId.class;
 
@@ -50,7 +62,28 @@ public class JdbcPostgresSequenceGeneratorTest {
         final SequenceNumber sequenceNumber = jdbcPostgresSequenceGenerator.nextFor(given);
 
         // Then
-        assertThat(sequenceNumber).isEqualTo(new SequenceNumber(1L));
+        final List<String> sequences = new ArrayList<>();
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement ps = connection.prepareStatement(
+                     // language=sql
+                     """
+                                 SELECT sequence_schema, sequence_name
+                                 FROM information_schema.sequences
+                                 ORDER BY sequence_schema, sequence_name;
+                             """);
+             final ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                String schema = rs.getString("sequence_schema");
+                String sequence = rs.getString("sequence_name");
+                sequences.add(schema + "." + sequence);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        assertAll(
+                () -> assertThat(sequences).containsExactly("todotaking_todo.seq_todochecklistid",
+                        "todotaking_todo.seq_todoid"),
+                () -> assertThat(sequenceNumber).isEqualTo(new SequenceNumber(1L)));
     }
 
     @Test
