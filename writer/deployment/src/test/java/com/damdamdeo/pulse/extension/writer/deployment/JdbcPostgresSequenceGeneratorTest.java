d0d88855adaa5149759bae1cffdd1d52b109670c
damien <damien.clementdhuart@gmail.com>
2026-05-16 21:52:11 +0200

    creational owned by with sequence generator

--- writer/deployment/src/test/java/com/damdamdeo/pulse/extension/writer/deployment/JdbcPostgresSequenceGeneratorTest.java
+++ writer/deployment/src/test/java/com/damdamdeo/pulse/extension/writer/deployment/JdbcPostgresSequenceGeneratorTest.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-// FCK
+
 class JdbcPostgresSequenceGeneratorTest {
 
     @RegisterExtension
