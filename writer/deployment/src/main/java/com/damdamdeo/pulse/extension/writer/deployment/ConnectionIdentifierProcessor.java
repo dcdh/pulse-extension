52e7451ab62b354300905feee27999396473a03e
damien <damien.clementdhuart@gmail.com>
2026-06-06 23:58:41 +0200

    add missing capability oidc condition on ConnectionIdentifierAssociation

--- writer/deployment/src/main/java/com/damdamdeo/pulse/extension/writer/deployment/ConnectionIdentifierProcessor.java
+++ writer/deployment/src/main/java/com/damdamdeo/pulse/extension/writer/deployment/ConnectionIdentifierProcessor.java
@@ -3,16 +3,20 @@ package com.damdamdeo.pulse.extension.writer.deployment;
 import com.damdamdeo.pulse.extension.core.connectionidentifier.ConnectionIdentifierAssociation;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.processor.DotNames;
+import io.quarkus.deployment.Capabilities;
+import io.quarkus.deployment.Capability;
 import io.quarkus.deployment.annotations.BuildStep;
 
 public class ConnectionIdentifierProcessor {
 
     @BuildStep
-    AdditionalBeanBuildItem additionalBeans() {
-        return AdditionalBeanBuildItem.builder()
-                .addBeanClasses(ConnectionIdentifierAssociation.class)
-                .setDefaultScope(DotNames.APPLICATION_SCOPED)
-                .setUnremovable()
-                .build();
+    AdditionalBeanBuildItem additionalBeans(final Capabilities capabilities) {
+        final AdditionalBeanBuildItem.Builder builder = AdditionalBeanBuildItem.builder();
+        if (capabilities.isPresent(Capability.OIDC)) {
+            builder.addBeanClasses(ConnectionIdentifierAssociation.class)
+                    .setDefaultScope(DotNames.APPLICATION_SCOPED)
+                    .setUnremovable();
+        }
+        return builder.build();
     }
 }
