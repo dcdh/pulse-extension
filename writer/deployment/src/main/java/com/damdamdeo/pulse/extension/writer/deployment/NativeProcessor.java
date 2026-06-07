71091a4f70141b24f6be8fb0439cddea2410364a
damien <damien.clementdhuart@gmail.com>
2026-06-07 15:42:11 +0200

    rework native reflection - do it on CreationalCommand and use ReflectiveHierarchyBuildItem to apply nested reflection

--- writer/deployment/src/main/java/com/damdamdeo/pulse/extension/writer/deployment/NativeProcessor.java
+++ writer/deployment/src/main/java/com/damdamdeo/pulse/extension/writer/deployment/NativeProcessor.java
@@ -3,54 +3,68 @@ package com.damdamdeo.pulse.extension.writer.deployment;
 import com.damdamdeo.pulse.extension.core.AggregateId;
 import com.damdamdeo.pulse.extension.core.AggregateRoot;
 import com.damdamdeo.pulse.extension.core.command.Command;
+import com.damdamdeo.pulse.extension.core.command.CreationalCommand;
 import com.damdamdeo.pulse.extension.core.event.Event;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveHierarchyBuildItem;
+
+import java.util.List;
 
 public class NativeProcessor {
 
     @BuildStep
-    ReflectiveClassBuildItem registerCommands(final CombinedIndexBuildItem combinedIndexBuildItem) {
-        final String[] classes = combinedIndexBuildItem.getIndex()
+    List<ReflectiveHierarchyBuildItem> registerCommands(final CombinedIndexBuildItem combinedIndexBuildItem) {
+        return combinedIndexBuildItem.getIndex()
                 .getAllKnownImplementations(Command.class)
                 .stream()
                 .map(classInfo -> classInfo.name().toString())
-                .toArray(String[]::new);
-        return ReflectiveClassBuildItem.builder(classes)
-                .classes().constructors().fields().methods().publicConstructors().build();
+                .map(className -> ReflectiveHierarchyBuildItem.builder(className).constructors(true)
+                        .fields(true).methods(true).ignoreNested(false).build())
+                .toList();
     }
 
     @BuildStep
-    ReflectiveClassBuildItem registerEvents(final CombinedIndexBuildItem combinedIndexBuildItem) {
-        final String[] classes = combinedIndexBuildItem.getIndex()
+    List<ReflectiveHierarchyBuildItem> registerCreationalCommands(final CombinedIndexBuildItem combinedIndexBuildItem) {
+        return combinedIndexBuildItem.getIndex()
+                .getAllKnownImplementations(CreationalCommand.class)
+                .stream()
+                .map(classInfo -> classInfo.name().toString())
+                .map(className -> ReflectiveHierarchyBuildItem.builder(className).constructors(true)
+                        .fields(true).methods(true).ignoreNested(false).build())
+                .toList();
+    }
+
+    @BuildStep
+    List<ReflectiveHierarchyBuildItem> registerEvents(final CombinedIndexBuildItem combinedIndexBuildItem) {
+        return combinedIndexBuildItem.getIndex()
                 .getAllKnownImplementations(Event.class)
                 .stream()
                 .map(classInfo -> classInfo.name().toString())
-                .toArray(String[]::new);
-        return ReflectiveClassBuildItem.builder(classes)
-                .classes().constructors().fields().methods().publicConstructors().build();
+                .map(className -> ReflectiveHierarchyBuildItem.builder(className).constructors(true)
+                        .fields(true).methods(true).ignoreNested(false).build())
+                .toList();
     }
 
     @BuildStep
-    ReflectiveClassBuildItem registerAggregateRoots(final CombinedIndexBuildItem combinedIndexBuildItem) {
-        final String[] classes = combinedIndexBuildItem.getIndex()
+    List<ReflectiveHierarchyBuildItem> registerAggregateRoots(final CombinedIndexBuildItem combinedIndexBuildItem) {
+        return combinedIndexBuildItem.getIndex()
                 .getAllKnownSubclasses(AggregateRoot.class)
                 .stream()
                 .map(classInfo -> classInfo.name().toString())
-                .toArray(String[]::new);
-        return ReflectiveClassBuildItem.builder(classes)
-                .classes().constructors().fields().methods().publicConstructors().build();
+                .map(className -> ReflectiveHierarchyBuildItem.builder(className).constructors(true)
+                        .fields(true).methods(true).ignoreNested(false).build())
+                .toList();
     }
 
     @BuildStep
-    ReflectiveClassBuildItem registerAggregateIds(final CombinedIndexBuildItem combinedIndexBuildItem) {
-        final String[] classes = combinedIndexBuildItem.getIndex()
+    List<ReflectiveHierarchyBuildItem> registerAggregateIds(final CombinedIndexBuildItem combinedIndexBuildItem) {
+        return combinedIndexBuildItem.getIndex()
                 .getAllKnownImplementations(AggregateId.class)
                 .stream()
                 .map(classInfo -> classInfo.name().toString())
-                .toArray(String[]::new);
-        return ReflectiveClassBuildItem.builder(classes)
-                .classes().constructors().fields().methods().publicConstructors().build();
+                .map(className -> ReflectiveHierarchyBuildItem.builder(className).constructors(true)
+                        .fields(true).methods(true).ignoreNested(false).build())
+                .toList();
     }
 }
