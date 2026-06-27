d22e92a5e6cf36472c274a5b518c58a98364f80d
damien <damien.clementdhuart@gmail.com>
2026-06-27 17:25:18 +0200

    Update comment on Nullable annotation

--- core/src/main/java/com/damdamdeo/pulse/extension/core/Nullable.java
+++ core/src/main/java/com/damdamdeo/pulse/extension/core/Nullable.java
@@ -3,7 +3,9 @@ package com.damdamdeo.pulse.extension.core;
 import java.lang.annotation.*;
 
 /**
- * Marks an optional or new field as nullable. In an event a new field is always nullable regarding the past!
+ * Marks an optional or new field as nullable.
+ * Can be a business requirement.
+ * In an event a new field is always nullable regarding the past!
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
