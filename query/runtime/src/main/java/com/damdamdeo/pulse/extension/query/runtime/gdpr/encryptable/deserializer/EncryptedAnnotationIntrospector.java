a4fa2d4568de505a0995a5fd3e81f38f1105fd07
damien <damien.clementdhuart@gmail.com>
2026-07-26 20:46:37 +0200

    use a delegate to use build in Jackson converters

--- query/runtime/src/main/java/com/damdamdeo/pulse/extension/query/runtime/gdpr/encryptable/deserializer/EncryptedAnnotationIntrospector.java
+++ query/runtime/src/main/java/com/damdamdeo/pulse/extension/query/runtime/gdpr/encryptable/deserializer/EncryptedAnnotationIntrospector.java
@@ -1,18 +1,13 @@
 package com.damdamdeo.pulse.extension.query.runtime.gdpr.encryptable.deserializer;
 
-import com.damdamdeo.pulse.extension.core.encryption.*;
+import com.damdamdeo.pulse.extension.core.encryption.DecryptionService;
 import com.damdamdeo.pulse.extension.query.runtime.PulseQueryConfig;
 import com.damdamdeo.pulse.extension.query.runtime.gdpr.Encrypted;
 import com.damdamdeo.pulse.extension.query.runtime.gdpr.encryptable.serializer.EncryptablePropertyWriter;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.PropertyName;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.introspect.Annotated;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 
-import java.io.IOException;
 import java.util.Objects;
 
 public final class EncryptedAnnotationIntrospector extends JacksonAnnotationIntrospector {
@@ -37,49 +32,8 @@ public final class EncryptedAnnotationIntrospector extends JacksonAnnotationIntr
     @Override
     public Object findDeserializer(final Annotated annotated) {
         if (annotated.hasAnnotation(Encrypted.class)) {
-            if (annotated.getRawType() == String.class) {
-                return new StdDeserializer<String>(String.class) {
-                    @Override
-                    public String deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException {
-                        return new String(decrypt(p));
-                    }
-                };
-            } else if (annotated.getRawType() == Long.class) {
-                return new StdDeserializer<Long>(Long.class) {
-                    @Override
-                    public Long deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException {
-                        return Long.valueOf(new String(decrypt(p)));
-                    }
-                };
-            } else if (annotated.getRawType() == Integer.class) {
-                return new StdDeserializer<Integer>(Integer.class) {
-                    @Override
-                    public Integer deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException {
-                        return Integer.valueOf(new String(decrypt(p)));
-                    }
-                };
-            } else if (annotated.getRawType() == Double.class) {
-                return new StdDeserializer<Double>(Double.class) {
-                    @Override
-                    public Double deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException {
-                        return Double.valueOf(new String(decrypt(p)));
-                    }
-                };
-            } else {
-                throw new UnsupportedOperationException("Unsupported type: " + annotated.getRawType());
-            }
+            return new EncryptedDeserializer(pulseQueryConfig, decryptionService);
         }
         return super.findDeserializer(annotated);
-    }
-
-    private byte[] decrypt(final JsonParser p) throws IOException {
-        try {
-            final MasterKey masterKey = new MasterKey(pulseQueryConfig.masterKey());
-            final EncryptedPayload encryptedPayload = new EncryptedPayload(p.getBinaryValue());
-            final DecryptedPayload decryptedPayload = decryptionService.decrypt(encryptedPayload, masterKey.toPassphrase());
-            return decryptedPayload.payload();
-        } catch (final DecryptionException e) {
-            throw JsonMappingException.from(p, "Unable to decrypt", e);
-        }
     }
 }
---
+++ query/runtime/src/main/java/com/damdamdeo/pulse/extension/query/runtime/gdpr/encryptable/deserializer/EncryptedDeserializer.java
@@ -0,0 +1,69 @@
+package com.damdamdeo.pulse.extension.query.runtime.gdpr.encryptable.deserializer;
+
+import com.damdamdeo.pulse.extension.core.encryption.*;
+import com.damdamdeo.pulse.extension.query.runtime.PulseQueryConfig;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
+import com.fasterxml.jackson.databind.util.TokenBuffer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class EncryptedDeserializer extends JsonDeserializer<Object> implements ContextualDeserializer {
+
+    private final PulseQueryConfig pulseQueryConfig;
+    private final DecryptionService decryptionService;
+    private final JsonDeserializer<Object> delegate;
+
+    public EncryptedDeserializer(final PulseQueryConfig pulseQueryConfig,
+                                 final DecryptionService decryptionService) {
+        this(pulseQueryConfig, decryptionService, null);
+    }
+
+    private EncryptedDeserializer(final PulseQueryConfig pulseQueryConfig,
+                                  final DecryptionService decryptionService,
+                                  final JsonDeserializer<Object> delegate) {
+        this.pulseQueryConfig = pulseQueryConfig;
+        this.decryptionService = decryptionService;
+        this.delegate = delegate;
+    }
+
+    @Override
+    public JsonDeserializer<?> createContextual(final DeserializationContext ctxt,
+                                                final BeanProperty property) throws JsonMappingException {
+        final JavaType type = (property != null) ? property.getType() : ctxt.getContextualType();
+        final JsonDeserializer<Object> defaultDelegate = ctxt.findContextualValueDeserializer(type, property);
+        return new EncryptedDeserializer(pulseQueryConfig, decryptionService, defaultDelegate);
+    }
+
+    @Override
+    public Object deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException {
+        final byte[] decryptedBytes = decrypt(p);
+        final String decryptedString = new String(decryptedBytes, StandardCharsets.UTF_8);
+
+        try (final TokenBuffer tb = new TokenBuffer(p.getCodec(), false)) {
+            tb.writeString(decryptedString);
+
+            try (final JsonParser delegateParser = tb.asParser(p)) {
+                delegateParser.nextToken();
+
+                if (delegate != null) {
+                    return delegate.deserialize(delegateParser, ctxt);
+                }
+                return delegateParser.getText();
+            }
+        }
+    }
+
+    private byte[] decrypt(final JsonParser p) throws IOException {
+        try {
+            final MasterKey masterKey = new MasterKey(pulseQueryConfig.masterKey());
+            final EncryptedPayload encryptedPayload = new EncryptedPayload(p.getBinaryValue());
+            final DecryptedPayload decryptedPayload = decryptionService.decrypt(encryptedPayload, masterKey.toPassphrase());
+            return decryptedPayload.payload();
+        } catch (final DecryptionException e) {
+            throw JsonMappingException.from(p, "Unable to decrypt", e);
+        }
+    }
+}
