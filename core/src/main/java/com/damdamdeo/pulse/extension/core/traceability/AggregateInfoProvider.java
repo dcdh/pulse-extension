e030153b30281aac84184f605b71bfc08b6de840
damien <damien.clementdhuart@gmail.com>
2026-09-02 23:34:56 +0200

    wip

---
+++ core/src/main/java/com/damdamdeo/pulse/extension/core/traceability/AggregateInfoProvider.java
@@ -0,0 +1,6 @@
+package com.damdamdeo.pulse.extension.core.traceability;
+
+public interface AggregateInfoProvider {
+    FCK je prends une liste d'aggregateId en entrée
+    je retourne l'aggregateId + le owner
+}
